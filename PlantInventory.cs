517b04a837c34c33ef1eb54c3363c25ae459081c
JudyF8 <judyfedu@gmail.com>
2026-04-28 23:40:09 -0500

    updated inventory gui + in game display

--- PlantInventory.cs
+++ PlantInventory.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ namespace final_project_PvZ;
 
 public class PlantInventory
 {
+    private const float Scale = 0.9f;
     private Dictionary<PlantType, int> plants;
     private Texture2D Texture;
     private FrameDefinition Backdrop;
@@ -43,13 +45,58 @@ public class PlantInventory
 
     public void Draw(SpriteBatch spriteBatch, SpriteFont font)
     {
+        List<FrameDefinition> rosterBox = new List<FrameDefinition>();
+
+        int displayX = 35;
+        Rectangle rect = new Rectangle(257, 462, 91, 105);
+
+        rosterBox.Add(new FrameDefinition
+        {
+            SourceRectangle = rect,
+            Size = new Point(rect.Width, rect.Height),
+            Offset = new Vector2(displayX, 22)
+        });
+
+        displayX += 81;
+        rect.X += rosterBox[0].SourceRectangle.Width;
+        rect.Width = 100;
+        for (int i = 1; i < 7; i++)
+        {
+            rosterBox.Add(new FrameDefinition
+            {
+                SourceRectangle = rect,
+                Size = new Point(rect.Width, rect.Height),
+                Offset = new Vector2(displayX, 22)
+            });
+            displayX += 91;
+            rect.X += rosterBox[i].SourceRectangle.Width;
+        }
+
+        spriteBatch.Draw(Texture, new Vector2(20, 10), Backdrop.SourceRectangle, Color.White,
+            0.0f, Vector2.Zero, Scale, SpriteEffects.None, 0.0f);
+        spriteBatch.Draw(Texture, new Vector2(668, 20), Hand.SourceRectangle, Color.White,
+            0.0f, Vector2.Zero, Scale, SpriteEffects.None, 0.0f);
+
+        
+        foreach (PlantType type in PlantType.GetValuesAsUnderlyingType<PlantType>())
+        {
+            int i = (int)type;
+            Color tint = (plants.ContainsKey(type) && plants[type] > 0) ? Color.White : Color.Gray;
+            spriteBatch.Draw(Texture, rosterBox[i].Offset, rosterBox[i].SourceRectangle, tint,
+                0.0f, Vector2.Zero, Scale, SpriteEffects.None, 0.0f);
+            spriteBatch.DrawString(font, $"({i+1})", rosterBox[i].Offset, Color.White);
+            
+            // inventory count
+            string text = (plants.ContainsKey(type)) ? plants[type].ToString() : "";
+            spriteBatch.DrawString(font, text,
+                new Vector2(rosterBox[i].Offset.X + rosterBox[i].SourceRectangle.Width/2,
+                    rosterBox[i].Offset.Y + 90), Color.White);
+        }
+        
+        // flat inventory list display
+        /*
         int x = 50;
         int y = 20;
-        
-        spriteBatch.Draw(Texture, new Vector2(20, 10), Backdrop.SourceRectangle, Color.White);
-        spriteBatch.Draw(Texture, new Vector2(35, 20), Roster.SourceRectangle, Color.White);
-        spriteBatch.Draw(Texture, new Vector2(740, 20), Hand.SourceRectangle, Color.White);
-        
 
         foreach (var kvp in plants)
         {
@@ -63,12 +110,8 @@ public class PlantInventory
                 new Vector2(x, y),
                 Color.White
             );
-            
-            // I need to rework this display to finish this part... hardcoded vals !
-            // x += 90;
-
-            y += 30;
         }
+        */
     }
 
     private string GetKeyNumber(PlantType type)
